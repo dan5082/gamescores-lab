a6dc0bd90025c4f7c1626500e4bcae14543d053e
Daniel Niewierowski <dan5082@gmail.com>
2016-09-13 16:45:40 -0500

    1st commit

---
+++ game scores.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+
+using namespace std;
+
+    int main (void)
+    {
+
+    }
+
+    class GameEntry
+    {
+    public:
+        GameEntry(const string & n ="", int s=0);
+        string getName() const;
+        int getScore() const;
+
+    private:
+        string name;
+        int score;
+    };
+
+    class node
+    {
+        Node*
+        GameEntry data;
+
+
+    };
